bb8d9c37192c0d369eca9f7baf29261cade93b32
Geon Oh <ohhh123@naver.com>
2018-05-22 15:57:11 +0900

    프로젝트 생성

---
+++ Travel_여행 갈래?/Travel_여행 갈래?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel_여행 갈래?
+//
+//  Created by 오건 on 2018. 5. 22..
+//  Copyright © 2018년 오건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
